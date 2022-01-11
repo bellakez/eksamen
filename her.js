296bf3562d3b377edd80c460da60ffdbdd334ea8
bellakez <bellakezia28@stud.kea.dk>
2022-01-11 09:16:13 +0100

    javascript, animation og lighthousetest2

--- her.js
+++ her.js
@@ -1,24 +1,25 @@
 
-
 window.addEventListener("load", sidenVises);
 
 // burgermenu //
 function sidenVises() {
   console.log("sidenVises");
-  console.log("windowResize");
-  window.addEventListener("resize", windowResize);
-  document.querySelector("#menuknap").addEventListener("click", toggleMenu);
-  document
-    .querySelector("#rumskib")
-    .classList.add("animationiteration", "flyv");
-}
 
+  //console.log("windowResize");
+  //window.addEventListener("resize", windowResize);
+  
+  //kalder på funktionen toogleMenu når man klikker på menuknappen
+  document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+  //kalder på funktionen startFlyv når man klikker
+  document.querySelector("#rumskib").addEventListener("click", startFlyv);
+}
+//forholder sig til at menuen skifter mellem at have klassen "hidden" og ikke
 function toggleMenu() {
   console.log("toggleMenu");
   document.querySelector("#menu").classList.toggle("hidden");
-
+//definere variabel let, som er når menuen har klassen "hidden"
   let erSkjult = document.querySelector("#menu").classList.contains("hidden");
-
+//ved at bruge variabel let til at skife mellem if or else med tegn herunder, når menuen er skjult 
   if (erSkjult == true) {
     document.querySelector("#menuknap").textContent = "☰";
   } else {
@@ -26,8 +27,9 @@ function toggleMenu() {
   }
 }
 
-//forholder sig til at skærmen er en bestemt størrelse
-function windowResize() {
-  console.log("windowResize");
-  let widthScreen = document.querySelector("#baggrund").clientWidth;
-}
+//starter flyv animationen som er defineret i tema4.css 
+function startFlyv() {
+ document
+    .querySelector("#rumskib")
+    .classList.add("animationiteration", "flyv");
+  }
